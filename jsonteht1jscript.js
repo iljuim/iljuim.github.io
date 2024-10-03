3c54b8cde68b0f567d2363e495da8fb56a2ddc82
Ilmari Immonen <ilmari.j.immonen@gmail.com>
2024-10-03 21:02:57 +0300

    hieman muutoksia jsoniin ja koodin puhdistamista

--- jsonteht1jscript.js
+++ jsonteht1jscript.js
@@ -9,10 +9,10 @@ fetch('https://raw.githubusercontent.com/iljuim/iljuim.github.io/refs/heads/main
     // Käsitellään muunnettu (eli JSON muotoinen) vastaus
     
     .then(function (responseJson) {
-        kerro(responseJson)
-        
+        kerro(responseJson);
+
         //document.getElementById("vastaus").innerHTML =
-        "<p>Jatketaan harjoitusta</p>";
+        //"<p>Jatketaan harjoitusta</p>";
         console.log(responseJson);
     
         // Testataan onnistuuko json-luku
@@ -29,43 +29,30 @@ fetch('https://raw.githubusercontent.com/iljuim/iljuim.github.io/refs/heads/main
     })
 
     function kerro(data) {
-        
+
         var teksti = "";
         
         teksti = "<h1>" + data.otsikko + "</h1>";
         teksti += "<p>" + data.kuvaus + "</p>";
         teksti += "<p><img src=" + data.kuva + " alt='kuva'></p>";
         teksti += "<h3>Opintojakso: " + data.opintojakso.nimi + 
-            " " + data.opintojakso.tunnus +
-            " " + data.opintojakso.opintopisteet + " op" + "</h3>";
+        " " + data.opintojakso.tunnus +
+        " " + data.opintojakso.opintopisteet + " op" + "</h3>";
 
-        teksti += "<ul>";
-
-        for(var i = 0; i < data.opintojakso.sisalto.length; i++) {
-            teksti += "<li>" + data.opintojakso.sisalto[i] + "</li>";
-        }
-
-        teksti += "</ul>";
-
-        teksti += "<p>" + data.tekniikat[0].linkki + "</p>";
-
-        teksti += "<ul>";
-        
-        for(var i = 0; i < data.tekniikat.length; i++) {
-            teksti += "<li>" + data.tekniikat[i].aihe + " " +
-                "<a href=data.tekniikat[i].linkki>" + 
-                data.tekniikat[i].linkki + "</a></li>";
-            console.log(teksti);        
-        }
-        // <a href="url">link text</a>
-
-        teksti += "</ul>"
-            
-        // tähän tulee muiden tietojen käsittelykohta
-
-        // teksti-muuttujan sisällön tulostus
+        teksti = "<p>" + data.sisalto[0] + "</p>";
+     
 
         document.getElementById("vastaus").innerHTML = teksti;
-
-
     }
+
+
+
+
+  
+
+
+
+    
+// tähän tulee muiden tietojen käsittelykohta
+//
+      //  // teksti-muuttujan sisällön tulostus
